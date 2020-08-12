5de3914d67f5213ec68f95b3ab220cf20cb96f63
Genoos <65106962+Genoos@users.noreply.github.com>
2020-08-12 22:22:35 +0530

    Update solver.py

--- solver.py
+++ solver.py
@@ -20,17 +20,17 @@ def solve(bo):
 
 
 def valid(bo, num, pos):
-    # Check row
+    # Check row whether num is already existing or not
     for i in range(len(bo[0])):
         if bo[pos[0]][i] == num and pos[1] != i:
             return False
 
-    # Check column
+    # Check column whether num is already existing or not
     for i in range(len(bo)):
         if bo[i][pos[1]] == num and pos[0] != i:
             return False
 
-    # Check box
+    # Check box whether num is already existing or not
     box_x = pos[1] // 3
     box_y = pos[0] // 3
 
@@ -41,7 +41,7 @@ def valid(bo, num, pos):
 
     return True
 
-
+# grid pattern
 def print_board(bo):
     for i in range(len(bo)):
         if i % 3 == 0 and i != 0:
@@ -61,6 +61,6 @@ def find_empty(bo):
     for i in range(len(bo)):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
-                return (i, j)  # row, col
+                return (i, j)  # row, col index
 
     return None
